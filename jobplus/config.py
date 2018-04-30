9afe4435b839f2fe9aa7b2080fcd1e3069de8ec4
Zhi-BeYourHero <763378665@qq.com>
2018-04-30 14:49:46 +0800

    Add

--- jobplus/config.py
+++ jobplus/config.py
@@ -7,7 +7,7 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql://root:xiaozhi520.@localhost:3306/jobplus?charset=utf8"
+    SQLALCHEMY_DATABASE_URI = "mysql://root@localhost:3306/jobplus?charset=utf8"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(BaseConfig):
